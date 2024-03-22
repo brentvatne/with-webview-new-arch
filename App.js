c5fead4e384e4a063297f6f033ecb6b69a1c407d
Brent Vatne <brentvatne@gmail.com>
2024-03-22 15:51:27 -0700

    Add example

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <WebView source={{ uri: 'https://reactnative.dev/' }} style={{ flex: 1, width: Dimensions.get('window').width }} />
       <StatusBar style="auto" />
     </View>
   );
